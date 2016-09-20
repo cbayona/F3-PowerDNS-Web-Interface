758df2a0ec71f6a83872b85d722d1b742db93613
MrSleeps <sleeps@trufflemonkey.co.uk>
2016-09-20 02:41:40 +0100

    Update install.sql

--- install/install.sql
+++ install/install.sql
@@ -8,12 +8,14 @@
 -- Table structure for table `w_domaindata`
 --
 
-CREATE TABLE `w_domaindata` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `w_domaindata` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `domainID` int(11) NOT NULL,
   `domainHash` varchar(255) NOT NULL,
-  `domainAdmin` int(11) DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `domainAdmin` int(11) DEFAULT '0',
+  `domainMaxRecords` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `w_domaindata`
@@ -46,61 +48,21 @@ INSERT INTO `w_userlevels` (`userLevelID`, `userLevelDesc`) VALUES
 -- Table structure for table `w_users`
 --
 
-CREATE TABLE `w_users` (
-  `userID` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `w_users` (
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
   `userEmail` varchar(255) NOT NULL,
   `userPassword` varchar(255) NOT NULL,
   `userName` varchar(255) NOT NULL,
-  `userAdminLevel` int(1) NOT NULL,
-  `userHash` varchar(255) NOT NULL,
-  `userEnabled` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Added By DNS Admin';
+  `userAdminLevel` int(1) NOT NULL DEFAULT '0',
+  `userHash` varchar(255) DEFAULT NULL,
+  `userEnabled` int(1) NOT NULL,
+  `userMaxDomains` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COMMENT='Added By DNS Admin';
 
 --
 -- Dumping data for table `w_users`
 --
 
-INSERT INTO `w_users` (`userID`, `userEmail`, `userPassword`, `userName`, `userAdminLevel`, `userHash`, `userEnabled`) VALUES
-(1, 'admin@admin.com', '$2y$10$5fEa3XaoXdl.npvVEz3o6uvMemRgeKIsu6gdyapbJ/5sdKeNl9gY.', 'You', 2, '', 1),
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `w_domaindata`
---
-ALTER TABLE `w_domaindata`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `w_userlevels`
---
-ALTER TABLE `w_userlevels`
-  ADD PRIMARY KEY (`userLevelID`);
-
---
--- Indexes for table `w_users`
---
-ALTER TABLE `w_users`
-  ADD PRIMARY KEY (`userID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `w_domaindata`
---
-ALTER TABLE `w_domaindata`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
---
--- AUTO_INCREMENT for table `w_userlevels`
---
-ALTER TABLE `w_userlevels`
-  MODIFY `userLevelID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `w_users`
---
-ALTER TABLE `w_users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+INSERT INTO `w_users` (`userID`, `userEmail`, `userPassword`, `userName`, `userAdminLevel`, `userHash`, `userEnabled`, `userMaxDomains`) VALUES
+(1, 'admin@admin.com', '$2y$10$5fEa3XaoXdl.npvVEz3o6uvMemRgeKIsu6gdyapbJ/5sdKeNl9gY.', 'You', 2, '', 1,0),
